753c159f0e215c869007ebb8450025680a2183f0
ryansuranjana <ryansuranjana23@gmail.com>
2025-06-18 23:49:27 +0800

    clean migration

--- prisma/migrations/20250618082529_fix_schema/migration.sql
+++ prisma/migrations/20250618082529_fix_schema/migration.sql
@@ -1,31 +1,19 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nickname` VARCHAR(30) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
-  - You are about to drop the column `name` on the `users` table. All the data in the column will be lost.
-  - You are about to drop the `group_members` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `groups` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `nickname` to the `users` table without a default value. This is not possible if the table is not empty.
-
-*/
--- DropForeignKey
-ALTER TABLE `group_members` DROP FOREIGN KEY `group_members_group_id_fkey`;
-
--- DropForeignKey
-ALTER TABLE `group_members` DROP FOREIGN KEY `group_members_user_id_fkey`;
-
--- AlterTable
-ALTER TABLE `users` DROP COLUMN `name`,
-    ADD COLUMN `nickname` VARCHAR(30) NOT NULL;
-
--- DropTable
-DROP TABLE `group_members`;
-
--- DropTable
-DROP TABLE `groups`;
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `circles` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `code` VARCHAR(10) NOT NULL,
     `invitation_code` VARCHAR(10) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
@@ -38,9 +26,9 @@ CREATE TABLE `circles` (
 
 -- CreateTable
 CREATE TABLE `circle_members` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_id` INTEGER NOT NULL,
-    `group_id` VARCHAR(191) NOT NULL,
+    `group_id` INTEGER NOT NULL,
 
     UNIQUE INDEX `circle_members_user_id_group_id_key`(`user_id`, `group_id`),
     PRIMARY KEY (`id`)
@@ -48,7 +36,7 @@ CREATE TABLE `circle_members` (
 
 -- CreateTable
 CREATE TABLE `schedules` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_id` INTEGER NOT NULL,
     `date` DATE NOT NULL,
     `start_time` DATETIME(0) NOT NULL,
